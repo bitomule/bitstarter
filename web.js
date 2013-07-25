df40c5a472a72ed47bb52113eb9e01abca826b88
David Collado <bitomule@gmail.com>
2013-07-25 11:33:21 +0000

    Error Fixed

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
     var fileStream = fs.readFileSync(index.html);
     var indexString = fileStream.toString();
     response.send(indexString);
-};
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
