953efb25d077ba6a9c4e481d0ba91302c0baa150
David Collado <bitomule@gmail.com>
2013-07-25 11:26:07 +0000

    First index render

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+    var fileStream = fs.readFileSync(index.html);
+    var indexString = fileStream.toString();
+    response.send(indexString);
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
